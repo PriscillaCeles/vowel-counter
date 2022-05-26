5ac14da53f4ef8f82093b9c77b8cc08bf9abb8be
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-05-26 19:08:47 -0300

    update JS

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 const $inputPhrase = document.querySelector('.input')
 const $displayResult = document.querySelector('.display-result')
 const $button= document.querySelector('.button')
+const $form= document.querySelector('.box')
 
 const vowels = 'aáàâãAÀÁÂÃeéêEÉÊiíIÍoóôõOÓÔÕuúUÚ'
 
-
-let vowelsCount = 0
-
-
-
 function vowelCounter () {
-  let phrase = $inputPhrase.value
+  let vowelsCount = 0
+  const phrase = $inputPhrase.value
+
   for(let i= 0; i < phrase.length; i++) {
-    for(let z= 0; z < vowels.length; z++){
-      if(phrase[i] === vowels[z]){
-        vowelsCount++
-      }
+    const isVowel = vowels.includes(phrase[i])
+    
+    if(isVowel)  vowelsCount++
     }
-  }
   return vowelsCount
 }
 
-$button.addEventListener('click', function(){
-  vowelCounter()
-  $displayResult.textContent = vowelsCount
-  vowelsCount = 0
+$form.addEventListener('submit', function(event){
+  event.preventDefault()
+  $displayResult.textContent = vowelCounter()
 })
 
 
